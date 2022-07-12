637923f2c3f161657885f23f63d1e0990c79b540
mzehnde <max.zehnder@uzh.ch>
2022-07-12 13:11:29 +0200

    changed reward font

--- src/Components/Reward.js
+++ src/Components/Reward.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Reward () {
     return <div>
         <h2>Congratulations! We sent the reward to your E-Mail address. Check it out for further instructions.</h2>
-        <h1> (Check your junk folder if you didn't get it.)</h1>
+        <h3> (Check your junk folder if you didn't get it.)</h3>
     </div>
 }
 export default Reward;
